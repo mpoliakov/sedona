f3697a60718425743309995fe2848491f98b8726
Max Poliakov <mpolyakov146@gmail.com>
2021-02-23 18:44:32 +0500

    updated packages to latest versions

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ gulp.task("css", function () {
       autoprefixer()
     ]))
     .pipe(gulp.dest("build/css"))
-    .pipe(csso())
+    //.pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
